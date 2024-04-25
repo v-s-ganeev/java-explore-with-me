113af32347616dd0fecfd2dc4e633e56f4b99981
g.s.ganeev <doroy93@yandex.ru>
2024-04-26 00:33:45 +0300

    fix: Правки в pom.xml

--- stats-service/client/src/main/java/ru/practicum/client/StatsServiceClient.java
+++ stats-service/client/src/main/java/ru/practicum/client/StatsServiceClient.java
@@ -1,4 +1,4 @@
-package ru.practicum.server.client;
+package ru.practicum.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
